d47ee1ced5603f85ea711e06f7a856e9dc325897
Adrien Chrome <adrien.thiery@gmail.com>
2014-11-10 12:14:46 -0500

    First step

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<title>An awesome poster of your medias, when you want!</title>
+        <meta charset="UTF-8">
+        <meta name="description" content="DekkoWall creates a poster of your media consumation (Books, Videos, Music) over a period YOU define.">
+        <meta name="keywords" content="Poster, Medias">
+        <meta name="author" content="@joydisee">
+        <link rel='stylesheet' id='base-css'  href='css/base.css' type='text/css' media='all' />
+        <link rel='stylesheet' id='main-css'  href='css/style.css' type='text/css' media='all' />
+</head>
+<body>
+	<header>
+		<h1>DekkoWall</h1>
+	</header>
---
+++ index.php
@@ -0,0 +1,21 @@
+<?php include( 'header.php' ); ?>
+<div class='bg'>
+</div>
+<section class='content-size'>
+	<div class='explain'>
+		<img src='img/poster.png' />
+		<ol>
+			<li>Connect your media services</li>
+			<li>Define a period</li>
+			<li>Get an awesome poster of the media you consumed during the period!</li>
+		</ol>
+	</div>
+</section>
+<section class='content-size'>
+	<div id='signup'>
+		<h2>Interested? Sign up!</h2>
+		<input type="email" id="mail" placeholder="e-mail address">
+                <button id="signup">Shoot me</button>
+	</div>
+</section>
+<?php include( 'footer.php' ); ?>
